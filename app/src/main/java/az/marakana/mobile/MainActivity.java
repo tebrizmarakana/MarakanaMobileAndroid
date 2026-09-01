af855e650d4a876f2a590446fccff751f8daaeab
tebrizmarakana <tebriz.mirzeyev@gmail.com>
2026-09-01 14:54:46 +0400

    Add files via upload

--- app/src/main/java/az/marakana/mobile/MainActivity.java
+++ app/src/main/java/az/marakana/mobile/MainActivity.java
@@ -32,9 +32,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.codescanner.GmsBarcodeScanner;
-import com.google.android.gms.codescanner.GmsBarcodeScannerOptions;
-import com.google.android.gms.codescanner.GmsBarcodeScanning;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
 import com.google.mlkit.vision.barcode.common.Barcode;
 
 import org.json.JSONArray;
